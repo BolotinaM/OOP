f479b7e9ab34bb3638144fbd10c97aee0b7772d6
Mariia <bolotina.23reg@yandex.ru>
2023-01-29 21:34:23 +0300

    Написали решение задачи № 2

---
+++ 2Pract/Program.cs
@@ -0,0 +1,25 @@
+﻿// "Перегрузка". Вариант 4
+
+// В следующих задачах перегрузить операции: «*» на случай произведения пар чисел 
+// (a1, b1) и (a2, b2): (a1, b1) * (a2, b2) = (a1 a2 – b2 b1, a1 b2 + a2 b1).
+
+class Programm {
+
+
+     static double Mult (double a1, double b1, double a2, double b2) {
+
+        return a1 * a2 - b2 * b1; 
+        }
+        
+     static int Mult (int a1, int b1, int a2, int b2) {
+        
+        return  a1 * b2 + a2 * b1; 
+        }
+
+    static void Main (string[] args) {
+
+        int Result = Mult(8, 7, 2, 3);
+
+        Console.WriteLine(Result);
+    }  
+}
